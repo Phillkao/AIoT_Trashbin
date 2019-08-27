0dfc8bdbbe5d43232732b2d9a4d42412d9b8d9db
Phillkao <48813692+Phillkao@users.noreply.github.com>
2019-08-27 19:46:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from IotDeviceControl import *
+from PiDeviceControl import *
     
 if __name__ == '__main__':
     IotDeviceState_TrashCan100Per_On()
     IotDeviceState_TrashCan100Per_Off()
     IotDeviceState_TrashCount_PlusOne()
+	
+	ChoInBo_D = PiDevice_GetChoInBo_Distance()
