2fee0435093ad1213b5930381f1a554883d360f7
Phillkao <48813692+Phillkao@users.noreply.github.com>
2019-08-27 23:33:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ if __name__ == '__main__':
     IotDeviceState_TrashCan100Per_Off()
     IotDeviceState_TrashCount_PlusOne()
 
-    ChoInBo_D = PiDevice_GetChoInBo_Distance()
-    print(ChoInBo_D)
+    while(1):
+        ChoInBo_D = PiDevice_GetChoInBo_Distance()
+        print(ChoInBo_D)
+        ChoInBo_D = PiDevice_GetChoInBo_Distance_2()
+        print(ChoInBo_D)
 
     PiDevice_GetCamera_Picture()
