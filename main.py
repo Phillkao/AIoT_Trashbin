ab1fbbf4958e4e439fee7f09fece64f6c8e922ca
haha <haha4ni@hotmail.com>
2019-08-27 15:07:25 +0100

    Camera OK

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from IotDeviceControl import *
 from PiDeviceControl import *
-    
+
 if __name__ == '__main__':
     IotDeviceState_TrashCan100Per_On()
     IotDeviceState_TrashCan100Per_Off()
     IotDeviceState_TrashCount_PlusOne()
-	
+
     ChoInBo_D = PiDevice_GetChoInBo_Distance()
     print(ChoInBo_D)
-	
-    PiDevice_GetCamera_Picture():
+
+    PiDevice_GetCamera_Picture()
