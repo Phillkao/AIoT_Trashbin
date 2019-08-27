31251a6606d2db88123f131103cf4985739c030d
haha <haha4ni@hotmail.com>
2019-08-27 17:46:06 +0100

    4fuMaDa

--- PiDeviceControl.py
+++ PiDeviceControl.py
@@ -74,3 +74,25 @@ def PiDevice_GetCamera_Picture():
     camera.resolution = (224, 224)
     time.sleep(5) # Camera warm-up time
     camera.capture('trash.png')
+
+
+#####################################
+import time
+ 
+CONTROL_PIN = 17
+PWM_FREQ = 50
+STEP=15
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(CONTROL_PIN, GPIO.OUT)
+ 
+pwm = GPIO.PWM(CONTROL_PIN, PWM_FREQ)
+pwm.start(0)
+
+def angle_to_duty_cycle(angle=0):
+    duty_cycle = (0.05 * PWM_FREQ) + (0.19 * PWM_FREQ * angle / 180)
+    return duty_cycle
+
+def PiDevice_Move4FuMata(angle):
+    dc = angle_to_duty_cycle(angle)
+    pwm.ChangeDutyCycle(dc)
--- main.py
+++ main.py
@@ -5,11 +5,24 @@ if __name__ == '__main__':
     IotDeviceState_TrashCan100Per_On()
     IotDeviceState_TrashCan100Per_Off()
     IotDeviceState_TrashCount_PlusOne()
-
+    '''
     while(1):
         ChoInBo_D = PiDevice_GetChoInBo_Distance()
         print(ChoInBo_D)
         ChoInBo_D = PiDevice_GetChoInBo_Distance_2()
         print(ChoInBo_D)
-
+    '''
+    '''
     PiDevice_GetCamera_Picture()
+    from keras.models import load_model
+    import numpy as np
+    import cv2
+    
+    image = cv2.imread("trash.png")
+    model = load_model('output.h5')
+    print(np.argmax(model.predict(image),1))
+    '''
+    PiDevice_Move4FuMata(0)
+    time.sleep(2)
+    PiDevice_Move4FuMata(180)
+    time.sleep(2)
